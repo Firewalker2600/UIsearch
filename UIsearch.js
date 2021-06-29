f7e18c3ef05564e2da0dc2c6d7a6c14597595a44
Josef Rezek <josef.rezek@gmail.com>
2021-06-30 00:21:14 +0200

    Final Push

--- UIsearch.js
+++ UIsearch.js
@@ -1,99 +1,93 @@
 (
 	function (nsObj, undefined) {
 		let $ = jQuery;
-
 		const keyA = "2c32bf3eb06b30af5f8208481aea3e8b"; //demo key
 		const keyB = "f599f68840eadc23261239307f05ab24"; //test key
 		const keyC = "638cf5af3a1030ba2ff3fe9b5cdd828a"; //prod key
 		const interval = 500;
+		const limit = 10;
+		const about_limit = 40;
 		let typingTimer;
 		let currentPage = 1;
-		let limit = 10;
 		let total = 0;
 		let url;
 		let loading = false;
-		const client = new AddSearchClient(keyC);
-		client.setPaging(1, limit, "relevance", "desc");
+		const client = new AddSearchClient(keyB);
 		nsObj.doSearch = function() {
-			//console.log("search-button clicked");
+			client.setPaging(1, limit, "relevance","desc");
 			client.search($("#beca_search_input").val(), nsObj.displayResults);
 		};
-
-		nsObj.cb = function(res) {
-			console.log(res);
-		}
 		nsObj.hideResults = function() {
 			$("#beca_results_container").hide();
-			$("#beca_search_input").css("border-radius", "26px");
+		}
+		nsObj.enter = function () {
+			if($(".active").length) {
+				window.location.href = $(".active").find("a").prop("href");
+			} else {
+				nsObj.doSearch();
+			}
 		}
 		nsObj.moveUp = function () {
-			//console.log("Up Arrow Pressed");
-
-			if($("#beca_results_container").is(":hidden")){
+			if($("#beca_results_container").is(":hidden")) {
 				return;
 			} else if($(".beca_grid_container:first-of-type").hasClass("active")) {
 				$(".beca_grid_container:first-of-type").removeClass("active");
-				$("#beca_search_input").focus();
 			} else {
-				$("._beca_grid_container active").removeClass("active").prev().addClass("active");
+				$(".active").removeClass("active").prev().addClass("active");
+				if($(".active").prev().length) {
+					$(".active").prev()[0].scrollIntoView();
+				}
 			}
 		}
 		nsObj.moveDown = function () {
-			console.log("Down Arrow Pressed");
-		}
-		nsObj.toggleActive = function (element) {
-			url = element.find("a").prop("href");
-			element.toggleClass("active");
-			$(".active").on("click", function () {
-				window.location.href = url;
-			});
-			$(".active").on("keypress", function (e) {
-				if(e.keyCode === 13) {
-					window.location.href = url;
-				}
-			});
-		}	
-
-		// --- zobrazovaci funkce pro konstrukci šablony
-
-		nsObj.displayResults = function(res) {
-			$("#beca_results_container").show();
-			$('#beca_results_list').empty();
-			nsObj.appendResults(res);
+			if($("#beca_results_container").is(":hidden")) {
+				return;
+			} else if($(".active").length) {
+				$(".beca_grid_container.active").removeClass("active").next().addClass("active").prev()[0].scrollIntoView();
+			} else {
+				$(".beca_grid_container:first-of-type").addClass("active");
+			}
 		}
 		nsObj.hasMoreResults = function () {
-			return ((currentPage - 1) * limit + 1) < total;
+			return total > limit + 1
+				? ((currentPage - 1) * limit + 1) < total
+				: false
+			;
 		}
 		nsObj.loadResults = async function () {
-			loading = true;
 			if(nsObj.hasMoreResults()) {
+				loading = true;
 				client.nextPage();
 				await client.search($("#beca_search_input").val(), nsObj.appendResults);
 				loading = false;
 			}
-			
-		}
+		}	
+		nsObj.displayResults = function(res) {
+			$("#beca_results_container").show();
+			$('#beca_results_list').empty().scrollTop(0);
+			nsObj.appendResults(res);
+		}		
 		nsObj.appendResults = function (res) {
-			total = res.total_hits || total;
-			currentPage = res.page || currentPage;
+			total = res.total_hits || 0;
+			currentPage = res.page || 1;
 			$("#beca_number_of_results").empty().append(
 				(total > 0)
-					? `${currentPage * limit} z ${total} výsledků.`
-					: `<h6>Žádný výsledek nenalezen.</h6>`
+					? `${(currentPage -1) * limit + 1} - ${currentPage * Math.min(limit, total)} z ${total} výsledků.`
+					: `<p>Žádný výsledek nenalezen.</p>`
 			);
 			if(total > 0) {
 				res.hits.forEach(nsObj.buildRow);
-				$(".beca_loader_wrapper").detach();
-				$("#beca_results_list").append(`
-					<div class="beca_loader_wrapper">
-						<div class="beca_loader">
-							<div></div><div></div><div></div><div></div>
-						</div>
-					</div>	
-				`);
-				$(".beca_grid_container").hover(function () {
-					nsObj.toggleActive($(this))
-				});													
+				$(".beca_grid_container")
+					.on("mouseover", function () {
+						if($(".active").length) {
+							$(".active").removeClass("active");
+						}
+						$(this).addClass("active");
+					})
+					.on("click", function () {
+						window.location.href = $(this).find("a").prop("href");
+					})
+				;													
 				$(".beca_results_about").each(function () {
 					let wordArray = $(this).html().split(' ');
 					while($(this).outerHeight() > $(this).offsetHeight) {
@@ -101,15 +95,28 @@
 						$(this).html(wordArray.join(' ') + '...');
 			 		}
 				});
-				$("#beca_results_list").on("scroll", function() {
-					if ($(this).scrollTop() == $(this).prop("scrollHeight") - $(this).height() && nsObj.hasMoreResults() && loading === false) {
-						nsObj.loadResults();
-					}
-				});
+				$(".beca_loader_wrapper").detach();
+				$("#beca_results_list")
+					.append(`
+						<div class="beca_loader_wrapper">
+							${nsObj.hasMoreResults()
+								?	`<div class="beca_loader">
+										<div></div><div></div><div></div><div></div>
+									</div>`
+								: 	total > 0 ? '<div id="beca_end_of_list">Konec seznamu</div>' : ''
+							}	
+						</div>
+					`)
+					.on("scroll", function() {
+						if ($(this).scrollTop() == $(this).prop("scrollHeight") - $(this).height() && nsObj.hasMoreResults() && loading === false) {
+							nsObj.loadResults();
+						}
+					})
+				;
 			}
 		}
 		nsObj.buildRow = function(hit) {
-			const about = hit.custom_fields.about ? nsObj.regFirstWords(hit.custom_fields.about, 40) : '';
+			const about = hit.custom_fields.about ? nsObj.regFirstWords(hit.custom_fields.about, about_limit) : '';
 			const categories = hit.custom_fields.categories ? nsObj.assignType(hit.custom_fields.categories) : '';		
 			const courses = hit.custom_fields.courses ?? '';
 			return $("#beca_results_list").append(`
@@ -138,9 +145,10 @@
 					</div>
 			`);
 		}
-
+		nsObj.appendLoader = function () {
+	
+		}
 	// --- pomocne funkce AKA helpery
-
 		nsObj.regFirstWords = function(s, n) {
 			// ?: non-capturing subsequent sp+word.Change {} if you want to require n instead of allowing fewer
 			return s.replace(/\s+/g," ").split(/(?=\s)/gi).slice(0, n).join('');
@@ -155,16 +163,17 @@
 					return '';	
 			}	 	
 		}
-		
 		nsObj.ready = function ($) {
 			$(function() {
-			/*$("#search-input").on('keydown', function(e) {
-				if(e.keyCode == '38' || e.keyCode == '40') {e.preventDefault();}
-			});*/
+				$("#beca_search_input").on('keydown keypress', function(e) {
+					if(e.keyCode == '38' || e.keyCode == '40') {
+						e.preventDefault();
+					}
+				});
 				$("#beca_search_input").on('keyup', function(e) {
 					switch(e.keyCode) {
 						case 13: // Enter Key
-							nsObj.doSearch();
+							nsObj.enter();
 							break;
 						case 27: //Esc Key
 							nsObj.hideResults();
@@ -186,14 +195,8 @@
 					e.stopPropagation();
 				});
 				$(document).on("click", nsObj.hideResults);
-				$("#nextPage").on("click", function () {
-					console.log("Next Page clicked!");
-					client.nextPage();
-					console.log();
-				});
 			});
 		}	
 	} (window.UIsearch = window.UIsearch || {})
 );
 UIsearch.ready($);
-
